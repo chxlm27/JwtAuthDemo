6ea8c35675d33d5cf49b59679594268a0ef66348
vlad <vlad@DESKTOP-SADLB8C>
2024-09-23 22:27:37 +0300

    Revert

---
+++ Role.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace JwtAuthDemo
+{
+    public class Role : IdentityRole<int>
+    {
+        public List<User> Users { get; set; } = new List<User>();
+    }
+}
---
+++ User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace JwtAuthDemo
+{
+    public class User : IdentityUser<int>
+    {
+        public List<Role> Roles { get; set; } = new List<Role>();
+    }
+}
